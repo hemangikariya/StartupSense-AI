a23bc3d37a73a649425684b670ebb17c39b7130a
Hemangi Kariya <hemangikariya.work@gmail.com>
2026-06-15 19:35:00 +0530

    commit

--- backend/main.py
+++ backend/main.py
@@ -57,4 +57,4 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("backend.main.py:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
